e66510551aaf6f33958b1551b7d3c9dc1955adeb
KanishkaCU <kanishkacu888@gmail.com>
2026-05-16 12:29:27 +0530

    entries

--- Problem2.py
+++ Problem2.py
@@ -1,15 +1,13 @@
 s = input()
 original = s
 
-for i in s:
-    if i == " ":
-        s.remove(i)
+s.replace(" ","")
         
-print(s)
+
 
 s1 = s[::-1]
 
-print(s1)
+
 
 if original == s1:
     print("True")
